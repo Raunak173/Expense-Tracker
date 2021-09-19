f4c811f53e561826b6397b5840c1de58116effb1
Raunak173 <raunakagarwal173@gmail.com>
2021-09-19 12:46:59 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import './App.css';
-import Home from './Pages/Home/Home';
-import AddExpense from "./Pages/AddExpense/AddExpense"
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-
 function App() {
-  return (
-    <Router>
-      <div className="header">
-        <h1>Expense Tracker</h1>
-      </div>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/add-exp' component={AddExpense}/>
-      </Switch>
-      <div className="footer">
-        <p>Developed by Raunak Agarwal</p>
-      </div>
-    </Router>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
